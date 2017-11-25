efc8ed6758879f3ee34567df7a7ebf71515788c7
jjf539 <jared.john.french@gmail.com>
2017-11-24 23:00:30 -0500

    Create str_rev.py

---
+++ Text/str_rev.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+user_str = raw_input("Enter a string to be reversed: ")
+rev_string = user_str[::-1] 					#splicing string by -1 while leaving off beg/end will reverse string
+print rev_string
